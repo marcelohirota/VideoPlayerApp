ac14c140951ee53399370182da964183075c26d4
Marcelo Hirota <marcelohirota@gmail.com>
2023-05-02 16:26:06 +0930

    Initial commit

---
+++ app/src/main/java/com/mh/videoplayerapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mh.videoplayerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val videoView = findViewById<VideoView>(R.id.videoView1)
+    }
+}
